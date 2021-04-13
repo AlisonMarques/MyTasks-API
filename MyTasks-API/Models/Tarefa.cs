51395cba032949474e6d3b94d3f125cc9f3870f9
AlisonMarques <alison-marques@outlook.com.br>
2021-04-13 11:27:00 -0300

    Nova migration para sincronizacao da API e aPP

--- MyTasks-API/Models/Tarefa.cs
+++ MyTasks-API/Models/Tarefa.cs
@@ -1,17 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyTasks_API.Models
 {
     public class Tarefa
     {
-        public int Id { get; set; }
+        //Id da tarefa no banco da Aplicação
+        public int IdTarefaApi { get; set; }
+
+        //id da tarefa no banco do Aplicativo mobile ou web
+        public int IdTarefaApp { get; set; }
+        
         public string Titulo { get; set; }
         public DateTime DataHora { get; set; }
         public string Local { get; set; }
         public string Descricao { get; set; }
         public string Tipo { get; set; }
         public bool Concluido { get; set; }
+        
+        //Vai ser para exclusão lógica
+        public bool Excluido { get; set; }
+        
         public DateTime Criado { get; set; }
         public DateTime Atualizado { get; set; }
 
